650156274a6a1e4973e33deb0c0d7568449bdd21
broutincharles-ai <broutincharles@gmail.com>
2026-07-12 22:03:39 +0200

    Add files via upload

--- evaluer/impact/assets/js/impact-config.js
+++ evaluer/impact/assets/js/impact-config.js
@@ -6,6 +6,6 @@
 window.IAHT_CONFIG = {
   aggregateEndpoint: "",
   contactEmail: "santetravailia@gmail.com",
-  appVersion: "0.7.3",
+  appVersion: "0.7.4",
   privacyPolicyUrl: "../../confidentialite/"
 };
